8caa86dc093675200f9c4da154cdd4b3b0dcac76
victormmontanari <victor9000@gmail.com>
2024-04-28 11:17:59 -0300

    Criação dos componentes e das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { PgPrincipalComponent } from './pg-principal/pg-principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PgPrincipalComponent},
+  { path: 'contato', component: ContatoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pg-principal/pg-principal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pg-principal',
+  templateUrl: './pg-principal.component.html',
+  styleUrls: ['./pg-principal.component.scss']
+})
+export class PgPrincipalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
